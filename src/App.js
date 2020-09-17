dff2db898c26096b916a96b58f1fbfef00421fa0
sadat <sadat.himel@gmail.com>
2020-09-17 21:58:41 +0600

    set logged in user in state, display logged in user info

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import * as firebase from "firebase/app";
 import "firebase/auth";
-import firebaseConfig from './firebase.config';
+import firebaseConfig from "./firebase.config";
 
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [user, setUser] = useState({
+    isSignIn: false,
+    name: "",
+    email: "",
+    photo: "",
+  });
 
   const provider = new firebase.auth.GoogleAuthProvider();
   const handleSignIn = () => {
-    firebase.auth().signInWithPopup(provider)
-    .then(res=>{
-      const {displayName, photoURL, email} = res.user;
-      console.log(displayName,email,photoURL);
-    })
-  }
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((res) => {
+        const { displayName, photoURL, email } = res.user;
+        const isSignedInUser = {
+          isSignIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL,
+        };
+        setUser(isSignedInUser);
+        console.log(displayName, email, photoURL);
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log(err.message);
+      });
+  };
 
   return (
     <div className="App">
-     <button onClick={handleSignIn}>Sign in</button>
+      <button onClick={handleSignIn}>Sign in</button>
+      {user.isSignIn && (
+        <div>
+          <p>Welcome, {user.name}</p>
+          <p>Your email: {user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      )}
     </div>
   );
 }

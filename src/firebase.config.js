10763854119d4196797cee0b98bd8b5a2d0b3fe4
sadat <sadat.himel@gmail.com>
2020-09-17 21:25:52 +0600

    sign in with google ,open google ligin app

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDiw3AdGgr5lfJwyaOjcDqgx6vKOU1cpRA",
+  authDomain: "ema-john-simple-9c1c6.firebaseapp.com",
+  databaseURL: "https://ema-john-simple-9c1c6.firebaseio.com",
+  projectId: "ema-john-simple-9c1c6",
+  storageBucket: "ema-john-simple-9c1c6.appspot.com",
+  messagingSenderId: "761735781175",
+  appId: "1:761735781175:web:c125bd7360d25720e798b2",
+};
+
+export default firebaseConfig;
